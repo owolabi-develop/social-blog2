9d6ab250c16ee61cae40cf132ff477c08533a215
owolabi-develop <owolabidevelop84@gmail.com>
2022-09-14 18:52:21 -0800

    turn off debug mode to off

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,9 +26,9 @@ env = environ.Env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['developer-news-porter.herokuapp.com']
 
 
 # Application definition
