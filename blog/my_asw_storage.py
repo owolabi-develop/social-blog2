1956d655db08da93cb645d40d1eebfcb0e21b5da
owolabi-develop <owolabidevelop84@gmail.com>
2023-01-20 09:40:56 -0800

    add cnd file

--- blog/my_asw_storage.py
+++ blog/my_asw_storage.py
@@ -2,11 +2,13 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class MediaStorage(S3Boto3Storage):
-    bucket_name = 'developer-news-porter'
+    bucket_name = 'social-blog'
     location = 'media'
     default_acl = 'public-read'
     file_overwrite = False
 
 class StaticStorage(S3Boto3Storage):
-    bucket_name = 'developer-news-porter'
+    bucket_name = 'social-blog'
     location = 'static'
+    default_acl = 'public-read'
+    file_overwrite = False
