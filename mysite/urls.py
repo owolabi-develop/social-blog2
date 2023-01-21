4400f6ce8ade7ad4ad1dd8c116a608a100c2f4ee
owolabi-develop <owolabidevelop84@gmail.com>
2023-01-20 16:06:42 -0800

    add default file

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('BlogAdmin/', admin.site.urls),
     path("",include('blog.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
